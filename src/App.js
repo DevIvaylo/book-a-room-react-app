1c66e502d8544cba20145a7f7b74d2201a6938da
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-13 18:41:03 +0100

    router setup

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import Home from "./components/Home";
 import Rooms from "./components/Rooms";
 import SingleRoom from "./components/SingleRoom";
+import {Switch, Route} from 'react-router-dom'
 import Error from "./components/Error";
+
 
 function App() {
     return (
-        <>
-            <Home/>
-            <Rooms/>
-            <SingleRoom/>
-            <Error/>
-        </>
+        <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/rooms/' component={Rooms}/>
+            <Route exact path='/rooms/:id/' component={SingleRoom}/>
+            <Route component={Error}/>
+        </Switch>
     );
 }
 
