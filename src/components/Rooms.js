e619238f23dc847144b7c3b1d880794062150471
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-13 17:39:39 +0100

    components setup

---
+++ src/components/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rooms = () => {
+    return (
+        <>
+            <h1>Rooms page</h1>
+        </>
+    )
+};
+
+export default Rooms;
---
+++ src/components/SingleRoom.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class SingleRoom extends Component {
+    render() {
+        return (
+            <>
+                <h1>Single room page</h1>
+            </>
+        );
+    }
+}
