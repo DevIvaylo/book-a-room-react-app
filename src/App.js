d5fce05b3274a4e5791a14cf35d2052827604099
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-14 12:45:24 +0100

    implementing navbar

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import Rooms from "./components/Rooms";
 import SingleRoom from "./components/SingleRoom";
 import {Switch, Route} from 'react-router-dom'
 import Error from "./components/Error";
+import Navbar from "./components/Navbar";
 
 
 function App() {
     return (
-        <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/rooms/' component={Rooms}/>
-            <Route exact path='/rooms/:id/' component={SingleRoom}/>
-            <Route component={Error}/>
-        </Switch>
+        <>
+            <Navbar/>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/rooms/' component={Rooms}/>
+                <Route exact path='/rooms/:id/' component={SingleRoom}/>
+                <Route component={Error}/>
+            </Switch>
+        </>
     );
 }
 
