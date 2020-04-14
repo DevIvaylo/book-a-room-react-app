5ea36ace537474cac02f1031e6c47b5edb0c9104
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-14 13:09:10 +0100

    adding header component to error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from "./Header";
 
 const Error = () => {
     return (
         <>
-            <h1>Error page</h1>
+            <Header/>
         </>
     )
 };
