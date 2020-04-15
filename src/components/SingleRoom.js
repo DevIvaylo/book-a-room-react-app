81638022279971b1b56c7bebae22a545efb171b3
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-15 17:14:44 +0100

    impementing dynamic styled component using style components library + completing the single room component

--- src/components/SingleRoom.js
+++ src/components/SingleRoom.js
@@ -1,10 +1,79 @@
 import React, {Component} from 'react';
+import defaultImg from '../images/room-1.jpeg';
+import Banner from "./shared/Banner";
+import {Link} from "react-router-dom";
+import {RoomContext} from "../Context";
+import StyledHeader from "./shared/StyledHeader";
 
 export default class SingleRoom extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultImg
+        }
+    }
+
+    static contextType = RoomContext;
+
+    // componentDidMount() {
+    // }
+
     render() {
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        console.log(room);
+        if (!room) {
+            return <div className="error">
+                <h3>no such room could be found...</h3>
+                <Link to='/rooms' className='btn-primary'>
+                    back to rooms
+                </Link>
+            </div>
+        }
+
+        // obj destructuring
+        const {name, description, capacity, size, price, extras, pets, breakfast, images} = room;
+
+        // array destructuring
+        const [mainImage, ...theRestOfTheImages] = images;
+        console.log(theRestOfTheImages);
+
         return (
             <>
-                <h1>Single room page</h1>
+                <StyledHeader img={mainImage || this.state.defaultImg}>
+                    <Banner title={`${name} room`}>
+                        <Link to='/rooms' className='btn-primary'>back to rooms</Link>
+                    </Banner>
+                </StyledHeader>
+                <section className='single-room'>
+                    <div className="single-room-images">
+                        {theRestOfTheImages.map((item, index) => <img src={item} key={index} alt={name}/>)}
+                    </div>
+                    <div className="single-room-info">
+                        <article className='desc'>
+                            <h3>details</h3>
+                            <p>{description}</p>
+                        </article>
+                        <article className='info'>
+                            <h3>info</h3>
+                            <h6>price : £{price}</h6>
+                            <h6>size : {size} SQFT</h6>
+                            <h6>max capacity : {capacity > 1 ? `${capacity} people` : `${capacity} person`}</h6>
+                            <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+                            <h6>{breakfast && 'free breakfast included'}</h6>
+                        </article>
+                    </div>
+                </section>
+                <section className='room-extras'>
+                    <h6>extras</h6>
+                    <ul className="extras">
+                        {extras.map((item, index) => {
+                            return <li key={index}>- {item}</li>
+                        })}
+                    </ul>
+                </section>
             </>
         );
     }
